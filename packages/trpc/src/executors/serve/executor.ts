a7fe0b448d19ee85b76c188c10fd8a5ee2e07987
Zack DeRose <zack.derose@gmail.com>
2023-01-23 21:06:55 -0700

    adding trpc local plugin

---
+++ packages/trpc/src/executors/serve/executor.ts
@@ -0,0 +1,82 @@
+import { ServeExecutorSchema } from './schema';
+import { ExecutorContext } from '@nrwl/devkit';
+import { ChildProcess, exec } from 'child_process';
+import * as chalk from 'chalk';
+
+const LARGE_BUFFER = 1024 * 1000000;
+
+export default async function runExecutor(
+  options: ServeExecutorSchema,
+  _context: ExecutorContext
+) {
+  const prefixSize =
+    Math.max(options.backendProject.length, options.frontendProject.length) + 3;
+  await startBackendServer(options, prefixSize);
+}
+
+async function startBackendServer(
+  options: ServeExecutorSchema,
+  targetPadSize: number
+) {
+  let frontendServerStarted = false;
+  return new Promise(() => {
+    const childProcess = exec(`npx nx serve ${options.backendProject}`, {
+      maxBuffer: LARGE_BUFFER,
+    });
+    process.on('exit', () => childProcess.kill());
+    process.on('SIGTERM', () => childProcess.kill());
+    prefixTerminalOutput(
+      childProcess,
+      chalk.bgGreen(padTargetName(options.backendProject, targetPadSize))
+    );
+    childProcess.stdout.on('data', (data) => {
+      if (!frontendServerStarted && data.includes('No errors found.')) {
+        startFrontendServer(options.frontendProject, targetPadSize);
+        frontendServerStarted = true;
+      }
+    });
+  });
+}
+
+async function startFrontendServer(
+  frontendProject: string,
+  targetPadSize: number
+) {
+  return new Promise(() => {
+    const childProcess = exec(`npx nx serve ${frontendProject}`, {
+      maxBuffer: LARGE_BUFFER,
+    });
+    process.on('exit', () => childProcess.kill());
+    process.on('SIGTERM', () => childProcess.kill());
+    prefixTerminalOutput(
+      childProcess,
+      chalk.bgBlue(padTargetName(frontendProject, targetPadSize))
+    );
+  });
+}
+
+function prefixTerminalOutput(cp: ChildProcess, prefix: string) {
+  function logWithPrefix(data: string) {
+    if (!data) {
+      return;
+    }
+    console.log(
+      data
+        .split('\n')
+        .map((line) => `${prefix} ${line}`)
+        .join('\n')
+    );
+  }
+  cp.stdout.on('data', logWithPrefix);
+  cp.stdout.on('error', logWithPrefix);
+  cp.stderr.on('data', logWithPrefix);
+  cp.stderr.on('error', logWithPrefix);
+}
+
+function padTargetName(name: string, targetSize: number) {
+  const builder = [`${name}`];
+  builder.push(' '.repeat(Math.floor((targetSize - name.length) / 2)));
+  builder.unshift(' '.repeat(Math.ceil((targetSize - name.length) / 2)));
+  builder.push(' ');
+  return builder.join('');
+}
---
+++ packages/trpc/src/executors/serve/schema.d.ts
@@ -0,0 +1,4 @@
+export interface ServeExecutorSchema {
+  frontendProject: string;
+  backendProject: string;
+}
